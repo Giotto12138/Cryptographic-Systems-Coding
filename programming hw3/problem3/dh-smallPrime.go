fe5ff023daf1eae3ab0651897a000f268fe60011
Giotto12138 <705808548@qq.com>
2020-05-03 22:14:23 -0400

    smallPrimeDH version one

---
+++ programming hw3/problem3/dh-smallPrime.go
@@ -0,0 +1,191 @@
+// This program is to generate a small prime number to test,
+// q is 20 bits prime number and p is 21 bits prime number
+
+package main
+
+import (
+	"crypto/rand"
+	"fmt"
+	"io"
+	"math/big"
+	"os"
+)
+
+func check(e error) {
+	if e != nil {
+		panic(e)
+	}
+}
+
+/**
+ * check if a file exists
+ */
+func checkFileIsExist(filename string) bool {
+	var exist = true
+	if _, err := os.Stat(filename); os.IsNotExist(err) {
+		exist = false
+	}
+	return exist
+}
+
+// generate a 1023 bits prime number q
+func getPrime() *big.Int {
+
+	q, err := rand.Prime(rand.Reader, 20)
+	if err != nil {
+		fmt.Printf("Can't generate %d-bit prime: %v", 20, err)
+	}
+	return q
+}
+
+// check a big prime number
+func checkPrime(q *big.Int) bool {
+
+	// calculate how many bits the p has
+	if q.BitLen() != 20 {
+		fmt.Printf("%v is not %d-bit", q, 20)
+		return false
+	}
+	// 32 times of Miller Rabin Prime Number test were performed on P. If the method returns true,
+	// the probability that P is prime is 1 - (1 / 4) * * 32; otherwise, P is not prime
+	if !q.ProbablyPrime(32) {
+		fmt.Printf("%v is not prime", q)
+		return false
+	}
+
+	return true
+}
+
+// calculate a  random generator
+func generator(p *big.Int, q *big.Int) *big.Int {
+	var one *big.Int = big.NewInt(1)
+	var two *big.Int = big.NewInt(2)
+	//var g *big.Int = big.NewInt(2) // the generator to be checked, begin from 2
+	var temp *big.Int = new(big.Int)
+
+	// get a random number from 2 to p-1
+	var g *big.Int = new(big.Int)
+	var pMinusOne *big.Int = new(big.Int)
+	pMinusOne.Sub(p, one)                   // get p-1
+	g, _ = rand.Int(rand.Reader, pMinusOne) // get a random a from 1 to p-1
+
+	for true {
+		// the formula to check if it is a generator: g{(p-1)/n} mod p
+		// n is 2 or q
+		flag := 0 // if flag equals 2, it is a generator
+		var n *big.Int = new(big.Int)
+
+		for i := 0; i < 2; i++ {
+
+			if i == 0 {
+				n.Set(two)
+			} else {
+				n.Set(q)
+			}
+
+			temp.Sub(p, one)     // get p-1
+			temp.Div(temp, n)    // get (p-1)/n
+			temp.Exp(g, temp, p) // get g{(p-1)/n} mod p
+
+			//fmt.Println("temp: ", temp)
+
+			if temp.Cmp(one) != 0 { // if the result is not 1, it is good
+				flag = flag + 1
+			}
+
+		}
+
+		if flag == 2 { // this is a generator
+			break
+		}
+
+		// if not a generator, try a new one
+		g, _ = rand.Int(rand.Reader, pMinusOne)
+	}
+
+	return g
+
+}
+
+func main() {
+
+	args := os.Args
+
+	// check the arguments
+	if len(args) != 3 {
+		fmt.Println("Invalid input")
+		fmt.Println("The input should be: dh-alice1 <filename for message to Bob> <filename to store secret key>")
+		return
+	}
+	pubFile := args[1]
+	priFile := args[2]
+
+	var one *big.Int = big.NewInt(1)
+	var two *big.Int = big.NewInt(2)
+	var q *big.Int
+	var p *big.Int = new(big.Int)
+
+	// generate a 20 bits prime number
+	q = getPrime()
+
+	// check the prime q, if it is not qualified, generate a new q and check again until we have a qualified q
+	for true {
+		if checkPrime(q) {
+			break
+		} else {
+			q = getPrime()
+		}
+	}
+
+	// generate a safe prime number p based on q, p = q*2+1
+	//temp = q
+	p.Mul(q, two)
+	p.Add(p, one)
+
+	// fmt.Println("q: ", q)
+	// fmt.Println("p: ", p)
+
+	g := generator(p, q)
+
+	// fmt.Println("g: ", g)
+
+	// generate secret number a
+	var a *big.Int = new(big.Int)
+	var pMinusOne *big.Int = new(big.Int)
+	pMinusOne.Sub(p, one)                   // get p-1
+	a, _ = rand.Int(rand.Reader, pMinusOne) // get a random a from 1 to p-1
+
+	// get g^a mod p
+	var g_a *big.Int = new(big.Int)
+	g_a.Exp(g, a, p)
+
+	pubKey := "( " + p.String() + "," + g.String() + "," + g_a.String() + " )"
+	priKey := "( " + p.String() + "," + g.String() + "," + a.String() + " )"
+
+	var f1, f2 *os.File
+	var err1, err2 error
+
+	// write public key into the first file
+	if checkFileIsExist(pubFile) { // if the file exists, open it
+		f1, err1 = os.OpenFile(pubFile, os.O_RDWR|os.O_CREATE|os.O_TRUNC, 0644)
+	} else {
+		f1, err1 = os.Create(pubFile) // if the file doesn't exists, create it
+	}
+
+	check(err1)
+	_, err1 = io.WriteString(f1, pubKey)
+
+	// write private key into the second file
+	if checkFileIsExist(priFile) { // if the file exists, open it
+		f2, err2 = os.OpenFile(priFile, os.O_RDWR|os.O_CREATE|os.O_TRUNC, 0644)
+	} else {
+		f2, err2 = os.Create(priFile) // if the file doesn't exists, create it
+	}
+
+	check(err2)
+	_, err2 = io.WriteString(f2, priKey)
+
+	fmt.Println("( p,g,g^a mod p )")
+	fmt.Println(pubKey)
+
+}
---
+++ programming hw3/problem3/dl-brute.go
@@ -0,0 +1,97 @@
+package main
+
+import (
+	"fmt"
+	"io/ioutil"
+	"math/big"
+	"os"
+	"strings"
+)
+
+func check(e error) {
+	if e != nil {
+		panic(e)
+	}
+}
+
+/**
+ * check if a file exists
+ */
+func checkFileIsExist(filename string) bool {
+	var exist = true
+	if _, err := os.Stat(filename); os.IsNotExist(err) {
+		exist = false
+	}
+	return exist
+}
+
+// analyze the public key from Alice
+func pubAnalyze(pub string) (*big.Int, *big.Int, *big.Int) {
+
+	var p, g, g_a big.Int
+
+	// delete spaces in the string
+	pub = strings.Replace(pub, " ", "", -1)
+	// split the string with comma, get three parts
+	strSplit := strings.Split(pub, ",")
+	// get p from the first part
+	pStr := strings.Split(strSplit[0], "(")
+	// get g from the second part
+	gStr := strSplit[1]
+	// get g_a from the second part
+	g_aStr := strings.Split(strSplit[2], ")")
+
+	// convert string to *big.Int
+	p.SetString(pStr[1], 10)
+	g.SetString(gStr, 10)
+	g_a.SetString(g_aStr[0], 10)
+
+	return &p, &g, &g_a
+}
+
+func bruteForce(p, g, g_a *big.Int) {
+	var zero *big.Int = big.NewInt(0)
+	var one *big.Int = big.NewInt(1)
+	var pMinusOne = new(big.Int).Sub(p, one)
+	var x *big.Int = new(big.Int)
+	var temp *big.Int = new(big.Int)
+
+	// guess the x, beginning from p-1 to 1
+	for x.Set(pMinusOne); x.Cmp(zero) > 0; x.Sub(x, one) {
+		temp.Exp(g, x, p)
+
+		if temp.Cmp(g_a) == 0 {
+			break
+		}
+	}
+
+	fmt.Println("x is: ", x)
+
+}
+
+func main() {
+
+	args := os.Args
+
+	// check the arguments
+	if len(args) != 2 {
+		fmt.Println("Invalid input")
+		fmt.Println("The input should be: dl-brute <filename for inputs>")
+		return
+	}
+	AlicePub := args[1]
+
+	// read the file storing public key
+	text, err := os.Open(AlicePub)
+	if err != nil {
+		fmt.Println("Input File Error")
+		os.Exit(1)
+	}
+	pubStr, _ := ioutil.ReadAll(text)
+
+	// get p, g, g_a from the public key
+	p, g, g_a := pubAnalyze(string(pubStr))
+
+	bruteForce(p, g, g_a)
+
+}
